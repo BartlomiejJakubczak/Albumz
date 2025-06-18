5bcd418f2ad7d0a8950d536852c6fcf266afcdea
unknown <bjakubczak96@gmail.com>
2025-06-18 09:34:09 +0200

    modified admin page, learned how to override base templates

--- albumz_app/admin.py
+++ albumz_app/admin.py
@@ -2,5 +2,17 @@ from django.contrib import admin
 
 from .domain.models import Album, User
 
-admin.site.register(Album)
-admin.site.register(User)
+class AlbumInline(admin.TabularInline):
+    model = Album
+    extra = 3
+
+class UserAdmin(admin.ModelAdmin):
+    inlines = [AlbumInline]
+
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ["title", "artist", "add_date", "user"]
+    list_filter = ["add_date"]
+    search_fields = ["title", "artist"]
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Album, AlbumAdmin)
