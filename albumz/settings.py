c177532c7d38bf993b0fb1c5217b8181276d8692
BJakubczak <bjakubczak96@gmail.com>
2025-07-16 09:54:13 +0200

    got rid of debug toolbar as I'm not even using it yet, cleaned up venv and froze a base requirements.txt

--- albumz/settings.py
+++ albumz/settings.py
@@ -38,7 +38,6 @@ INTERNAL_IPS = [
 INSTALLED_APPS = [
     "albumz_app.apps.AlbumzAppConfig",
     "accounts.apps.AccountsConfig",
-    "debug_toolbar",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -49,7 +48,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
