d1b6ce373681ad90176e57a2b0404e4d4a1d19a6
BJakubczak <bjakubczak96@gmail.com>
2025-07-28 12:51:10 +0200

    updated pre-commit black version

--- albumz_app/domain/exceptions.py
+++ albumz_app/domain/exceptions.py
@@ -1,16 +1,22 @@
 class AlbumAlreadyOnWishlistError(Exception):
-    """Raised when trying to add an album to the wishlist that is already on wishlist."""
+    """Raised when trying to add an album to the wishlist
+    that is already on wishlist.
+    """
 
     pass
 
 
 class AlbumAlreadyInCollectionError(Exception):
-    """Raised when trying to add an album to the collection that is already in collection."""
+    """Raised when trying to add an album to the collection
+    that is already in collection.
+    """
 
     pass
 
 
 class AlbumDoesNotExistError(Exception):
-    """Raised when trying to remove an album that is not in current possession, nor on wishlist."""
+    """Raised when trying to remove an album that is not in current possession,
+    nor on wishlist.
+    """
 
     pass
